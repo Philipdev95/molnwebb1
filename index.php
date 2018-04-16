dce57bbf567068e47cef8c2d09eb9ab56e97d381
Philipdev95 <Philipdev95gmail.com>
2018-04-16 14:17:31 +0200

    New files

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 <?php
 require 'vendor/autoload.php';
+
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 $log = new Logger('Laboration 1');
 $log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
 
-echo $argv[1] . " is a cool name!";
+$name = $_GET['name'];
+$log->info($name);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Testing, testing</title>
+</head>
+<body>
+<?php
+  echo "Hello, " . $name;
+?>
+</body>
+</html>
+
+<?php
+/*require 'vendor/autoload.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('Laboration 1');
+$log->pushHandler(new StreamHandler('greetings.log', Logger::INFO));
+
+echo $argv[1] . " is a cool name!";*/
 
 ?>
