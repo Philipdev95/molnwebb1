37bae4de25d938fb05bee66bbab42cafb5f3f820
Philipdev95 <philipdev95@gmail.com>
2018-04-24 11:19:36 +0200

    html meta fix

--- unicorn/index.php
+++ unicorn/index.php
@@ -13,7 +13,7 @@
     <link rel="apple-touch-icon" href="img/favicon.PNG">
     <link rel="apple-touch-startup-image" href="img/favicon.PNG">
     <!--  links  -->
-    <link href="style.css" rel="stylesheet">
+    <!--link href="style.css" rel="stylesheet"-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!--  fonts  -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,600" rel="stylesheet">
@@ -31,6 +31,6 @@
   </div> <!-- /container -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <script src="script.js"></script>
+    <!--script src="script.js"></script-->
   </body>
 </html>
